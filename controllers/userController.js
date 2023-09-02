f0584704b6425b588e42e9e2005ccd37b2518ce5
khloeli <chloelihuimin@gmail.com>
2023-09-03 07:39:40 +0800

    add photo url to itinerary model

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,4 @@
 const BaseController = require("./baseController");
-
 class UserController extends BaseController {
   constructor(model, activitiesModel, usersModel, user_itinerariesModel) {
     super(model);
@@ -7,7 +6,6 @@ class UserController extends BaseController {
     this.usersModel = usersModel;
     this.user_itinerariesModel = user_itinerariesModel;
   }
-
   async getAllUser(req, res) {
     try {
       const allUsers = await this.usersModel.findAll({});
@@ -16,26 +14,22 @@ class UserController extends BaseController {
       return res.status(400).json({ error: true, msg: err.messages });
     }
   }
-
   async checkUser(req, res) {
     const { email } = req.body;
     try {
       const [currUser] = await this.usersModel.findOrCreate({
         where: { email: email },
       });
-
       return res.json(currUser);
     } catch (err) {
       return res.status(400).json({ error: true, msg: err.messages });
     }
   }
-
   async editUser(req, res) {
     try {
       let userToAdd = req.body;
       let userToReplace = req.params.userId;
       console.log(userToReplace);
-
       let userToEdit = await this.usersModel.findByPk(userToReplace);
       console.log(userToEdit);
       await userToEdit.update(userToAdd);
@@ -44,6 +38,21 @@ class UserController extends BaseController {
       return res.status(400).json({ error: true, msg: err.messages });
     }
   }
+  async getAllUserByItinerary(req, res) {
+    const { itineraryId } = req.params;
+    try {
+      const allUsers = await this.user_itinerariesModel.findAll({
+        where: { itineraryId: itineraryId },
+        include: [
+          {
+            model: this.usersModel,
+          },
+        ],
+      });
+      return res.json(allUsers);
+    } catch (err) {
+      return res.status(400).json({ error: true, msg: err.message });
+    }
+  }
 }
-
 module.exports = UserController;
