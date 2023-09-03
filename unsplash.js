3a51cca7aa50b27383906bb188f8cc3ce35fcd7c
khloeli <chloelihuimin@gmail.com>
2023-09-03 17:35:51 +0800

    add function unsplash

---
+++ unsplash.js
@@ -0,0 +1,35 @@
+const { createApi } = require("unsplash-js");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const unsplash = new createApi({
+  accessKey: process.env.UNSPLASH_ACCESS_KEY,
+});
+
+async function SearchPhotos(query) {
+  try {
+    const res = await unsplash.search.getPhotos({
+      query: query,
+      page: 1,
+      perPage: 1,
+    });
+
+    if (
+      res.response &&
+      res.response.results &&
+      res.response.results.length > 0
+    ) {
+      // console.log("alt_description", res.response.results[0].alt_description);
+      // console.log("url", res.response.results[0].urls.full);
+      return res.response.results[0].urls.full;
+    } else {
+      console.log(`No results found for query: ${query}`);
+    }
+  } catch (error) {
+    console.error(`An error occurred: ${error}`);
+  }
+}
+
+// SearchPhotos({ query: "Opera House, Sydney" });
+
+module.exports = SearchPhotos;
