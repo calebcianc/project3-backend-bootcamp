c1a625aa5182649a580afdc30487c45c6db8e267
khloeli <chloelihuimin@gmail.com>
2023-09-09 01:10:13 +0800

    add user to itinerary method

--- routers/itineraryRouter.js
+++ routers/itineraryRouter.js
@@ -8,33 +8,34 @@ class ItineraryRouter {
   routes() {
     router.get("/", this.controller.getAll.bind(this.controller));
 
+    // explore page
     router.get(
       "/explore",
       this.controller.getAllItineraryActivitiesPublic.bind(this.controller)
     );
-
     router.get(
       "/explore/:userId",
       this.controller.getAllItineraryActivitiesPublicUser.bind(this.controller)
     );
 
+    // userId
     router.get(
       "/:userId",
       this.controller.getAllItinerary.bind(this.controller)
     );
-
+    router.post(
+      "/:userId/:itineraryId",
+      this.controller.addUserToItinerary.bind(this.controller)
+    );
     router.get(
       "/:userId/:itineraryId",
       this.controller.getOneItineraryActivityByUser.bind(this.controller)
     );
-
     router.post("/new", this.controller.createItinerary.bind(this.controller));
-
     router.put(
       "/:userId/:itineraryId",
       this.controller.editItinerary.bind(this.controller)
     );
-
     router.delete(
       "/:userId/:itineraryId",
       this.controller.deleteItinerary.bind(this.controller)
